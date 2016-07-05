aef9f778f7025b3720fa8f2aac463503fcc503ab
Jason Shen <sikajs@gmail.com>
2016-07-05 22:36:03 +0800

    Add paperclip for image upload

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+	has_attached_file :image, styles: { medium: "400x600>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
