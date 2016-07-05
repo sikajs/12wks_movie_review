6d0f70ee52efb5a9e7b554b168eae7391e842313
Jason Shen <sikajs@gmail.com>
2016-07-05 22:07:51 +0800

    Add movie scaffold and users

---
+++ app/models/movie.rb
@@ -0,0 +1,6 @@
+class Movie < ActiveRecord::Base
+	belongs_to :user
+
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+end
